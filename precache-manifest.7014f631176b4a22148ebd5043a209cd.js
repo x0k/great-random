48e3f5956fa2397c0175ca5fc603ea8b6abdba0e
Roman <x0kill@ya.ru>
2018-12-31 00:11:16 +0300

    Updates

---
+++ precache-manifest.7014f631176b4a22148ebd5043a209cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b24f819cfe6c34c083b6",
+    "url": "/great-random/static/css/main.cfaa1687.chunk.css"
+  },
+  {
+    "revision": "b24f819cfe6c34c083b6",
+    "url": "/great-random/static/js/main.b24f819c.chunk.js"
+  },
+  {
+    "revision": "a5ab97e6c801a255d3a6",
+    "url": "/great-random/static/js/1.a5ab97e6.chunk.js"
+  },
+  {
+    "revision": "5e2471517ca9e8f4eaa3",
+    "url": "/great-random/static/js/runtime~main.5e247151.js"
+  },
+  {
+    "revision": "7d7ec7379f35879b47f805d31828336b",
+    "url": "/great-random/index.html"
+  }
+];
